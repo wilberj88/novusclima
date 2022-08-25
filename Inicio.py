2c31785c88394980bae3a97b55129ad0607cf9f7
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2022-08-25 07:49:21 -0500

    Create Inicio.py

---
+++ Inicio.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+# SETTING PAGE CONFIG TO WIDE MODE AND ADDING A TITLE AND FAVICON
+st.set_page_config(layout="wide", page_title="Novus Clima", page_icon="⛅")
+
+st.title('Novus Clima ⛅')
+st.header("Ahorra 💰 mientras salvamos juntos el planeta 🌎")
+
+st.write("Bienvenidos al futuro climático 👋")
+
+st.markdown(
+  """
+  En esta web encontrarás:
+  - 🔎 _    Diagnóstico de tu riesgo climático
+  - 🛒 _    Recomendación de Seguro y Planes de Mitigación y Adaptación
+  - ✍️ _     Propuesta Inteligente de Contrato
+  
+  Todo lo anterior basado en:
+  - Tecnología para la modelación de riesgos climáticos
+  - Tecnología para la neutralidad de carbono: Planes de Mitigación
+  - Tecnología para salvar el planeta: Innovación en toda la cadena energética y Planes de Adaptación
+  
+  EMPIEZA TU 🔎 DIAGNÓSTICO AHORA 🕰
+  """
+)
