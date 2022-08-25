6b87f8f4190e8ec4cc508e3181b65223b13b2b56
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2022-08-25 07:53:13 -0500

    Create 2_⚠️_ Riesgos.py

---
+++ pages/2_⚠️_ Riesgos.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title('Gracias a tu Diagnóstico🔎 sabemos que tu Matriz de Riesgos ⚠️ es:')
+st.write('1 año: XXX')
+st.write('5 años: YYYY')
+st.write('El portafolio de servicios personalizado 🛒 a tu necesidad🎯 es:')
+
+st.write('BLA BLA BLA')
+
+
+st.write('Empecemos ahora 🕰')
