beebe37fb493589179f2570bbf7a8a151aa8e509
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2023-02-22 13:01:48 -0500

    Update democonexsis.py

--- pages/democonexsis.py
+++ pages/democonexsis.py
@@ -10,3 +10,12 @@ st.set_page_config(layout="wide", page_title="Novus Clima", page_icon="⛅")
 st.title('Novus Clima ⛅ Demo by NovusTech+Exsis')
 st.header("¿Sabes cuánto te costaría la próxima crisis climática?🌎")
 st.write("Averígualo ahora 🕰")
+
+
+territorio = st.selectbox("Indica el Territorio",
+        ("Santander", "Tolima", "Caribe", "Pacífico", "Amazonas"),
+    )
+categoria = st.radio(
+        "Indica la crisis para la cual te quieres preparar👇 ",
+        options=['Inundación', 'Sequía','Incendios', 'Huracanes', 'Deslizamientos'],
+    )
