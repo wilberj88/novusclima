238feb7b538cd90469326291a82a79046527e40a
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2023-02-22 12:56:17 -0500

    Update democonexsis.py

--- pages/democonexsis.py
+++ pages/democonexsis.py
@@ -1,1 +1,12 @@
-streamlit as st
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pydeck as pdk
+
+
+# SETTING PAGE CONFIG TO WIDE MODE AND ADDING A TITLE AND FAVICON
+st.set_page_config(layout="wide", page_title="Novus Clima", page_icon="⛅")
+
+st.title('Novus Clima ⛅ Demo by NovusTech+Exsis')
+st.header("¿Sabes cuánto te costaría la próxima crisis climática?🌎")
+st.write("Averígualo ahora 🕰")
