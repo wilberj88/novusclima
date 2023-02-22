abbb108f8a7f5539713324279c58df87256403a1
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2023-02-22 13:08:39 -0500

    Update democonexsis.py

--- pages/democonexsis.py
+++ pages/democonexsis.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pydeck as pdk
-
+import plotly.figure_factory as ff
 
 # SETTING PAGE CONFIG TO WIDE MODE AND ADDING A TITLE AND FAVICON
 st.set_page_config(layout="wide", page_title="Novus Clima", page_icon="⛅")
