64b7f556b09ad6e440c49545d7d361334ae8aa80
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2023-07-26 18:52:43 -0500

    Update 2_⚠️_ Riesgos.py

--- pages/2_⚠️_ Riesgos.py
+++ pages/2_⚠️_ Riesgos.py
@@ -1,5 +1,67 @@
 import streamlit as st
 
+import altair as alt
+import streamlit as st
+from vega_datasets import data
+
+source = data.seattle_weather()
+
+scale = alt.Scale(
+    domain=["sun", "fog", "drizzle", "rain", "snow"],
+    range=["#e7ba52", "#a7a7a7", "#aec7e8", "#1f77b4", "#9467bd"],
+)
+color = alt.Color("weather:N", scale=scale)
+
+# We create two selections:
+# - a brush that is active on the top panel
+# - a multi-click that is active on the bottom panel
+brush = alt.selection_interval(encodings=["x"])
+click = alt.selection_multi(encodings=["color"])
+
+# Top panel is scatter plot of temperature vs time
+points = (
+    alt.Chart()
+    .mark_point()
+    .encode(
+        alt.X("monthdate(date):T", title="Date"),
+        alt.Y(
+            "temp_max:Q",
+            title="Maximum Daily Temperature (C)",
+            scale=alt.Scale(domain=[-5, 40]),
+        ),
+        color=alt.condition(brush, color, alt.value("lightgray")),
+        size=alt.Size("precipitation:Q", scale=alt.Scale(range=[5, 200])),
+    )
+    .properties(width=550, height=300)
+    .add_selection(brush)
+    .transform_filter(click)
+)
+
+# Bottom panel is a bar chart of weather type
+bars = (
+    alt.Chart()
+    .mark_bar()
+    .encode(
+        x="count()",
+        y="weather:N",
+        color=alt.condition(click, color, alt.value("lightgray")),
+    )
+    .transform_filter(brush)
+    .properties(
+        width=550,
+    )
+    .add_selection(click)
+)
+
+chart = alt.vconcat(points, bars, data=source, title="Seattle Weather: 2012-2015")
+
+tab1, tab2 = st.tabs(["Streamlit theme (default)", "Altair native theme"])
+
+with tab1:
+    st.altair_chart(chart, theme="streamlit", use_container_width=True)
+with tab2:
+    st.altair_chart(chart, theme=None, use_container_width=True)
+
 st.title('Gracias a tu Diagnóstico🔎 sabemos que tu Matriz de Riesgos ⚠️ es:')
 st.write('1 año: XXX')
 st.write('5 años: YYYY')
