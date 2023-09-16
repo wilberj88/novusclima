2e8fe0664cc8f8466908bb8fa56f012aecc0f8fd
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2023-09-15 20:41:39 -0500

    Update creditoverde.py

--- pages/creditoverde.py
+++ pages/creditoverde.py
@@ -186,45 +186,45 @@ if st.button('Calcular diagnóstico gratuito'):
     
     @st.experimental_memo
     def get_chart_25713448():
-    url = 'https://raw.githubusercontent.com/plotly/plotly.js/master/test/image/mocks/sankey_energy.json'
-    response = urllib.request.urlopen(url)
-    data = json.loads(response.read())
-
-    # override gray link colors with 'source' colors
-    opacity = 0.4
-    # change 'magenta' to its 'rgba' value to add opacity
-    data['data'][0]['node']['color'] = ['rgba(255,0,255, 0.8)' if color == "magenta" else color for color in data['data'][0]['node']['color']]
-    data['data'][0]['link']['color'] = [data['data'][0]['node']['color'][src].replace("0.8", str(opacity))
-                                        for src in data['data'][0]['link']['source']]
-
-    fig = go.Figure(data=[go.Sankey(
-        valueformat = ".0f",
-        valuesuffix = "TWh",
-        # Define nodes
-        node = dict(
-          pad = 15,
-          thickness = 15,
-          line = dict(color = "black", width = 0.5),
-          label =  data['data'][0]['node']['label'],
-          color =  data['data'][0]['node']['color']
-        ),
-        # Add links
-        link = dict(
-          source =  data['data'][0]['link']['source'],
-          target =  data['data'][0]['link']['target'],
-          value =  data['data'][0]['link']['value'],
-          label =  data['data'][0]['link']['label'],
-          color =  data['data'][0]['link']['color']
-    ))])
-
-    fig.update_layout(title_text="Energy forecast for 2050<br>Source: Department of Energy & Climate Change, Tom Counsell via <a href='https://bost.ocks.org/mike/sankey/'>Mike Bostock</a>",
-                      font_size=10)
-
-    tab1, tab2 = st.tabs(["Streamlit theme (default)", "Plotly native theme"])
-    with tab1:
-        st.plotly_chart(fig, theme="streamlit")
-    with tab2:
-        st.plotly_chart(fig, theme=None)
+        url = 'https://raw.githubusercontent.com/plotly/plotly.js/master/test/image/mocks/sankey_energy.json'
+        response = urllib.request.urlopen(url)
+        data = json.loads(response.read())
+    
+        # override gray link colors with 'source' colors
+        opacity = 0.4
+        # change 'magenta' to its 'rgba' value to add opacity
+        data['data'][0]['node']['color'] = ['rgba(255,0,255, 0.8)' if color == "magenta" else color for color in data['data'][0]['node']['color']]
+        data['data'][0]['link']['color'] = [data['data'][0]['node']['color'][src].replace("0.8", str(opacity))
+                                            for src in data['data'][0]['link']['source']]
+    
+        fig = go.Figure(data=[go.Sankey(
+            valueformat = ".0f",
+            valuesuffix = "TWh",
+            # Define nodes
+            node = dict(
+              pad = 15,
+              thickness = 15,
+              line = dict(color = "black", width = 0.5),
+              label =  data['data'][0]['node']['label'],
+              color =  data['data'][0]['node']['color']
+            ),
+            # Add links
+            link = dict(
+              source =  data['data'][0]['link']['source'],
+              target =  data['data'][0]['link']['target'],
+              value =  data['data'][0]['link']['value'],
+              label =  data['data'][0]['link']['label'],
+              color =  data['data'][0]['link']['color']
+        ))])
+    
+        fig.update_layout(title_text="Energy forecast for 2050<br>Source: Department of Energy & Climate Change, Tom Counsell via <a href='https://bost.ocks.org/mike/sankey/'>Mike Bostock</a>",
+                          font_size=10)
+    
+        tab1, tab2 = st.tabs(["Streamlit theme (default)", "Plotly native theme"])
+        with tab1:
+            st.plotly_chart(fig, theme="streamlit")
+        with tab2:
+            st.plotly_chart(fig, theme=None)
 
 
 st.write("""
